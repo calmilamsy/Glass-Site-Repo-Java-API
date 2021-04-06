e8cacac554df21175b59f36b5e7aa63532ec75ac
kyle cheeseman <bumbill00@gmail.com>
2021-04-06 21:07:48 +0100

    v0.7

--- src/main/java/net/glasslauncher/repo/api/mod/jsonobj/ValidModValues.java
+++ src/main/java/net/glasslauncher/repo/api/mod/jsonobj/ValidModValues.java
@@ -1,9 +1,11 @@
 package net.glasslauncher.repo.api.mod.jsonobj;
 
+import com.google.gson.annotations.SerializedName;
 import lombok.Getter;
 
 @Getter
 public class ValidModValues {
 
+    @SerializedName("valid_values")
     private ModValues validValues;
 }
